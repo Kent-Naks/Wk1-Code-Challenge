6428e09a2b7f61406c445bffd8db7539f37cfab8
Kent Eugine Kariuki <eugenekariuki@gmail.com>
2024-07-03 16:05:38 +0300

    Adds

---
+++ One-folder/Challenge-2/speedDetector.js
@@ -0,0 +1,21 @@
+// speedDetector.js
+
+function speedDetector(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed < speedLimit + kmPerPoint) {
+        console.log('Ok');
+        return;
+    }
+
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points >= 12) {
+        console.log('License suspended');
+    } else {
+        console.log(Points: ${points});
+    }
+}
+
+// Uncomment the line below to run the function
+// speedDetector(80);
