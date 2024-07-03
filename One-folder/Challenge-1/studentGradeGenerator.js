96683236a6793c1581fd9888967e49c8d52387f0
Kent Eugine Kariuki <eugenekariuki@gmail.com>
2024-07-03 16:35:15 +0300

    Adds

--- One-folder/Challenge-1/studentGradeGenerator.js
+++ One-folder/Challenge-1/studentGradeGenerator.js
@@ -14,17 +14,41 @@ function calculateGrade(mark) {
     }
 }
 
-function studentGradeGenerator() {
-    let marks = prompt('Enter student marks (0-100):');
-    marks = parseInt(marks);
+ // If running in browser environmemnt
+ if (typeof wondow !== 'underfined') {
+    marks = promt ('Enter student marks (0-100):');
+ }
+else {
+    //using readline for node.js or other non-browser envs
+    const read = required('readline').creatInterface({
+        input: ProcessingInstruction.stdin,
+        output: ProcessingInstruction.stdout
+    });
 
+    readline.question('Eter student mark(0-100): ',
+        input => {
+            marks = input;
+            readline.close();
+            processInput();
+        });
+
+    //Dealy execution until input is recieved
+    return;
+}
+
+
+processInput();
+
+function processInput() {
+
+marks=parseInt(marks);
     if (isNaN(marks) || marks < 0 || marks > 100) {
         console.log('Invalid input. Marks should be between 0 and 100.');
         return;
     }
 
-    let grade = calculateGrade(marks);
-    console.log(`Grade for ${marks} marks is ${grade}`);
+let grade = calculatedGrade(marks);
+console.log('Grade for ${marks} marks is ${grade}');
 }
 
 // Uncomment the line below to run the function
